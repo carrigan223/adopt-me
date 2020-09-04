f78df94073cca0ce08ff2b20d08e9c4288785705
Andrew Carrigan <carrigan223@gmail.com>
2020-09-04 07:48:11 -0700

    Custom Hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from './useDropdown'
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-  const [animal, setAnimal] = useState("dog");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
+
   /* 
     location is the current state while setLocation is an updater for the state
     so every time an event happens in my input it us updating state 
@@ -11,6 +16,11 @@ const SearchParams = () => {
     in our case useState. useState will always take in an array of the first index being the 
     current state and the second index being a function that updates that state. HOOKS NEVER GO INSIDE IF STATEMENTS
     OR FOR LOOPS OR ANY CONDITIONAL LOGIC.
+
+
+    we are also useing custom hooks for the animal and breed, we created a useDropdown hook which
+    we are importing from useDropdown.js. this is allowing us to pass the nessecary information to 
+    a generic dropdown component which we can then quickly and convenietly re-render
     */
 
   return (
@@ -26,20 +36,8 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(e) => setAnimal(e.target.value)}
-            onBlur={(e) => setAnimal(e.target.value)}
-          >
-            <option>All</option>
-            {ANIMALS.map((animal) => (
-              <option key={animal} value={animal}>{animal}</option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
---
+++ src/useDropdown.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const useDropdown = (label, defaultState, options) => {
+  const [state, setState] = useState(defaultState);
+  const id = `use-dropdown-${label.replace("", "").toLowerCase()}`;
+  const Dropdown = () => (
+    <label htmlFor={id}>
+      {label}
+      <select
+        id={id}
+        value={state}
+        onChange={(e) => setState(e.target.value)}
+        onBlur={(e) => setState(e.target.value)}
+        disabled={!options.length}
+      >
+        <option>All</option>
+        {options.map((item) => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+
+  return [state, Dropdown, setState];
+};
+
+export default useDropdown;
+
+/* 
+
+    The above code is a custom hook, we have created a custom hook called useDropdown. 
+    useDropdown is a reusable generic hook we can now use for our dropdowns.
+
+*/
