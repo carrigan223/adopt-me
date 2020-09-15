7268e2558591107ba4f63b3a0acc1af69bb8808c
Andrew Carrigan <carrigan223@gmail.com>
2020-09-15 02:07:14 -0700

    Adopt Me done

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ThemeContext from "./ThemeContext";
 import pet from "@frontendmasters/pet";
+import { navigate } from '@reach/router';
+import Modal from './Modal';
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 
@@ -21,11 +23,12 @@ class Details extends React.Component {
   
   */
 
-  state = { loading: true };
+  state = { loading: true, showModal: false };
   componentDidMount() {
     //throw new Error('lol'); //uncomment this line to test
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
+        url: animal.url,
         name: animal.name,
         animal: animal.type,
         location: `${animal.contact.address.city},
@@ -38,12 +41,15 @@ class Details extends React.Component {
     }, console.error);
   }
 
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+  adopt = () => navigate(this.state.url);
+
   render() {
     if (this.state.loading) {
       return <h1>Loading...</h1>;
     }
 
-    const { animal, breed, location, description, name, media } = this.state;
+    const { animal, breed, location, description, name, media, showModal } = this.state;
 
     return (
       <div className="details">
@@ -62,6 +68,20 @@ class Details extends React.Component {
             )}
           </ThemeContext.Consumer>
           ;<p>{description}</p>
+          {
+            showModal ? (
+              <Modal>
+                <div>
+            <h1>Would you like to adopt {name}?</h1>
+                    <div className='buttons'>
+                      <button onClick={this.adopt}>Yes</button>
+                      <button onClick={this.toggleModal}>No</button>
+                    </div>
+                  
+                </div>
+              </Modal>
+            ) : null
+          }
         </div>
       </div>
     );
